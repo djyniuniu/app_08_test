6c9919e3164d26bb554163687dca8e05498f92d2
djyniuniu <1356460629@qq.com>
2018-07-06 17:15:28 +0800

    修改脚本

--- Script/test_allure.py
+++ Script/test_allure.py
@@ -1,3 +1,16 @@
+import allure,pytest
+
 class Test_Allure(object):
+    @allure.step(title='登录')
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
     def test_001(self):
+        allure.attach('描述','输入用户名')
+        allure.attach('描述', '输入密码')
+        allure.attach('描述', '点击登录')
         assert 0
+
+    @allure.step(title='注册')
+    @pytest.allure.severity(pytest.allure.severity_level.TRIVIAL)
+    def test_002(self):
+        allure.attach('描述','点击注册')
+        assert 1
